c1b57b34fb1bc50bdf9510c70b06e97051524206
PabloGastonMedina <pablogastonmedina@gmail.com>
2024-05-04 12:40:50 -0300

    creacion del html y css

---
+++ app.js
@@ -0,0 +1,5 @@
+const contenedor = document.querySelector(".cajaBusqueda");
+const inputEscribir = document.querySelector("#ciudad");
+const btn = document.querySelector(".boton");
+
+btn.addEventListener("click", (e) => console.log(e.target.value));
